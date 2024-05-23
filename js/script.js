bd99acf2dbd0f6ad4bd238e6a446689a2cd7378a
sakanachqn <sakanachqn@gmail.com>
2024-05-23 11:29:24 +0900

    add

--- js/script.js
+++ js/script.js
@@ -17,4 +17,25 @@ $(function () {
     return false;
   });
 
+  let currentIndex = 0;
+
+function moveCarousel(direction) {
+    const carouselInner = document.querySelector('.carousel-inner');
+    const items = document.querySelectorAll('.carousel-item');
+    const itemWidth = items[0].clientWidth;
+    const maxIndex = items.length - 3;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = 0;
+    } else if (currentIndex > maxIndex) {
+        currentIndex = maxIndex;
+    }
+
+    const offset = -currentIndex * itemWidth;
+    carouselInner.style.transform = `translateX(${offset}px)`;
+  }
+
+
 });
